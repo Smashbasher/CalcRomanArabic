1bcb8623227c8cc82cf913f1b9d1bb17511a1107
Paul <106315598+Smashbasher@users.noreply.github.com>
2022-06-22 03:03:02 +0300

    Add files via upload

--- src/Input.java
+++ src/Input.java
@@ -1,13 +1,27 @@
 import java.util.Scanner;
 
 public class Input {
+    private static String extractValue(String quotedValue){
+        return quotedValue.replaceAll("\"", "");
+    }
     public static String[] read() throws Exception {
-        Scanner sc = new Scanner(System.in);
-        String input = sc.nextLine();
-        String[] parts = input.split(" ");
-        if (parts.length > 3) {
+        Scanner scnr = new Scanner(System.in);
+        String input = scnr.nextLine();
+        String[] values = input.split(" ");
+        if (values.length > 3) {
             throw new Exception("Больше двух аргументов");
         }
-        return parts;
+        boolean isInt = IntChecker.isIntValue(values[0]);
+        if (isInt == true){
+            throw new Exception("Операции, начинаемые с чисел недопустимы");
+        }
+        boolean inQuotes = StringQuotes.isValid(values);
+        if (inQuotes == false){
+            throw new Exception("Допускаются только строки в кавычках и целые числа");
+        }
+        values[0] = Input.extractValue(values[0]);
+        values[2] = Input.extractValue(values[2]);
+        return values;
     }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,12 @@
-import java.util.Arrays;
-
 public class Main {
-    public static void main(String[] args) throws Exception{
-        String[] parts = Input.read();
-        int[] nums = Parse.parse(parts);
-        char op = parts[1].charAt(0);
-        int result = Calculate.calc(nums[0], nums[1], op);
-        if (Parse.isRome) {
-            System.out.println(Convert.convertArabianToRoman(result));
-        }
-        if (Parse.isRome & result < 1) {
-            throw new Exception("Недопустимая операция");
-        }
-        else {
-            System.out.println(result);
-        }
+    public static void main(String[] args) throws Exception {
+        System.out.println("Введите выражение типа: \"а\"+\"b\", \"a\"-\"b\", \"a\"*x, \"a\"/x");
+        String[] values = Input.read();
+        String opr = values[1];
+        String result = Calc.calculate(values[0], values[2], opr);
+        int resultLength = result.length();
+        if (resultLength > 100){
+            throw new Exception("Ввод строковых значений больше 10 символов и чисел больше 10 не допускается");
+        } else System.out.println("\"" + result + "\"");
     }
 }
